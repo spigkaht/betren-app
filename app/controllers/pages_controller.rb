cc2853febbc7a40673f1538bf90b857fef4991ac
Benjamin Jackson <benj@betterrentals.com.au>
2024-07-29 16:23:39 +1000

    update mid invoice page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
+    @contracts = Contract.all
+    @customers = Customer.all
+    @items = Item.all
   end
 end

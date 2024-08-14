79f8bbbe3bf6569149176a7d08003a6f4f8b5f15
Benjamin Jackson <benj@betterrentals.com.au>
2024-08-14 12:11:33 +1000

    creating index page etc

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      fontSize: {
+        'xxs': '0.625rem', // 10px
+        'xxxs': '0.5rem',   // 8px
+      },
     },
   },
   plugins: [
--- db/secondary_schema.rb
+++ db/secondary_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_12_023808) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_14_001706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_12_023808) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
   end
 
 end

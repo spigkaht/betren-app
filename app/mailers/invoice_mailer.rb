153b6ca0dfb661aaaa79537c41d184dd3dc62776
Benjamin Jackson <benj@betterrentals.com.au>
2024-08-20 17:36:09 +1000

    heroku email scheduler update

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,10 +1,14 @@
 # app/jobs/generate_and_email_invoices_job.rb
 class InvoiceMailer < ApplicationMailer
-  default from: "benj@betterrentals.com.au"
+  default from: "Better Rentals <hello@betterrentals.com.au>"
 
   def send_invoice_email(invoice, pdf)
     @invoice = invoice
     attachments["Invoice ##{invoice.CNTR}.pdf"] = pdf
-    mail(to: "benj@betterrentals.com.au, bradf@betterrentals.com.au", subject: "Invoice ##{invoice.CNTR}")
+    mail(
+      to: "#{@invoice.customer.Email}",
+      bcc: "bradf@betterrentals.com.au",
+      subject: "Invoice ##{invoice.CNTR}"
+      )
   end
 end

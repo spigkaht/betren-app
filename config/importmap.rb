adb2fe71f1e6bd9b7649f9aadc45f5c519ea30ea
Benjamin Jackson <benj@betterrentals.com.au>
2024-09-03 09:02:14 +1000

    creating loading displays

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,8 +2,9 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.9
 pin "sortablejs" # @1.15.2
+pin "@stimulus-components/content-loader", to: "@stimulus-components--content-loader.js" # @5.0.0

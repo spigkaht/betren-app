b0532729bb4329346ab11dcd78de628d745d071f
Benjamin Jackson <benj@betterrentals.com.au>
2024-09-10 19:15:35 +1000

    heroku config updates

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,10 @@
-require 'cloudinary'
-require 'cloudinary/carrier_wave'
-
 CarrierWave.configure do |config|
   config.cache_storage = :file
-
-  config.cloudinary_credentials = {
-    cloud_name: ENV.fetch("CLOUDINARY_CLOUD_NAME"),
-    api_key: ENV.fetch("CLOUDINARY_API_KEY"),
-    api_secret: ENV.fetch("CLOUDINARY_API_SECRET")
+  config.fog_credentials = {
+    provider:              'Cloudinary',
+    cloud_name:            ENV.fetch("CLOUDINARY_CLOUD_NAME"),
+    api_key:               ENV.fetch("CLOUDINARY_API_KEY"),
+    api_secret:            ENV.fetch("CLOUDINARY_API_SECRET"),
   }
-  config.storage = :cloudinary
+  config.fog_directory  = ENV.fetch("CLOUDINARY_CLOUD_NAME")
 end

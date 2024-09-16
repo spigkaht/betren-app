86f65be07f07b5b78c1a60a1077010ba7a54c8ce
Benjamin Jackson <benj@betterrentals.com.au>
2024-09-17 09:06:53 +1000

    qr code scanning implementation

--- app/javascript/controllers/qr_code_scanner_controller.js
+++ app/javascript/controllers/qr_code_scanner_controller.js
@@ -9,17 +9,29 @@ export default class extends Controller {
 
   openScanner() {
     this.scannerDivTarget.classList.remove("hidden");
+    this.scannerDivTarget.classList.add("flex");
     this.formDivTarget.classList.add("hidden");
+    this.formDivTarget.classList.remove("flex");
     this.startCamera();
   }
 
   startCamera() {
     const video = this.videoTarget;
+    video.style.width = "75%";
+    video.style.height = "auto";
+
     navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
       .then(stream => {
         video.srcObject = stream;
         video.setAttribute("playsinline", true); // For iOS
         video.play();
+
+        // Wait for the video metadata to load before setting up the canvas and scrolling
+        video.addEventListener("loadedmetadata", () => {
+          this.setupCanvas();
+          this.scrollToBottom();
+        });
+
         this.hasScanner = true;
         requestAnimationFrame(this.scanQRCode.bind(this));
       })
@@ -28,15 +40,20 @@ export default class extends Controller {
       });
   }
 
+  setupCanvas() {
+    const video = this.videoTarget;
+    const canvas = this.canvasTarget;
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+  }
+
   scanQRCode() {
     const video = this.videoTarget;
     const canvas = this.canvasTarget;
 
-    // Ensure the video is fully loaded and has enough data before scanning
     if (video.readyState === video.HAVE_ENOUGH_DATA) {
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      const context = canvas.getContext("2d");
+      const context = canvas.getContext("2d", { willReadFrequently: true });
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
       const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
@@ -48,7 +65,6 @@ export default class extends Controller {
       }
     }
 
-    // Continue scanning if no valid code has been found
     requestAnimationFrame(this.scanQRCode.bind(this));
   }
 
@@ -56,7 +72,13 @@ export default class extends Controller {
     const video = this.videoTarget;
     video.srcObject.getTracks().forEach(track => track.stop()); // Stop the camera
     this.scannerDivTarget.classList.add("hidden"); // Hide the scanner
+    this.scannerDivTarget.classList.remove("flex");
     this.formDivTarget.classList.remove("hidden"); // Show the form again
+    this.formDivTarget.classList.add("flex");
     this.hasScanner = false;
+  }
+
+  scrollToBottom() {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
   }
 }

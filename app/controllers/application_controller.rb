5b1f01c603144d846591b3b6fd116f0729e9d8a4
Benjamin Jackson <benj@betterrentals.com.au>
2024-09-19 10:15:07 +1000

    ..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def not_found
+    render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,6 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  match '*path', to: 'application#not_found', via: :all
 end

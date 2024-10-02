a31af5473286ed7f8ea913fe248abcb98af91ad4
Benjamin Jackson <benj@betterrentals.com.au>
2024-10-02 14:09:30 +1000

    ..

--- app/models/phc_entry.rb
+++
@@ -1,2 +0,0 @@
-class PhcEntry < ApplicationRecord
-end
--- db/migrate/20241002015718_create_phc_entries.rb
+++
@@ -1,8 +0,0 @@
-class CreatePhcEntries < ActiveRecord::Migration[7.1]
-  def change
-    create_table :phc_entries do |t|
-
-      t.timestamps
-    end
-  end
-end

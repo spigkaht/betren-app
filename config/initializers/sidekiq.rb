f42f1d31d778bd6df0c7d38e35b7abc8ce32717d
Benjamin Jackson <benj@betterrentals.com.au>
2024-10-08 15:50:02 +1100

    heroku config update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,6 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.redis = { url: ENV['REDIS_URL'], size: 12, network_timeout: 5, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.average_scheduled_poll_interval = 1
 end
 
 Sidekiq.configure_client do |config|

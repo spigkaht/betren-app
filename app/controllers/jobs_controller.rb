0e215064c73994871f8cd3a49e70903edf4b6b54
Benjamin Jackson <benj@betterrentals.com.au>
2024-10-29 12:07:54 +1100

    fixing qol issues

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -108,6 +108,17 @@ class JobsController < ApplicationController
 
     if item
       @job = Job.new(item: item, template: template, store: current_user.store, last_return: last_return, last_contract: last_contract)
+
+      if params[:job_ids]
+        @jobs = Job.find(params[:job_ids])
+      end
+
+      @jobs.each do |job|
+        if job.item_num == item.NUM
+          redirect_to jobs_path, alert: 'Item is already on the list below' and return
+        end
+      end
+
       if @job.save
         redirect_to @job, notice: 'Job was successfully created.'
       else
